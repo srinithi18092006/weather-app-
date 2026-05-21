28e2b4ce80d5adec32354006581a10f7dc4238df
srinithi18092006 <srinithi18092006@gmail.com>
2026-05-21 19:32:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,110 +1,44 @@
-<<<<<<< HEAD
 let searchBtn = document.getElementById("searchBtn");
 
 searchBtn.addEventListener("click", function () {
 
-let city = document.getElementById("city").value;
+    let city = document.getElementById("city").value;
 
-if(city === ""){
-alert("Please enter city");
-return;
-}
+    if (city === "") {
+        alert("Please enter city");
+        return;
+    }
 
-let apiKey = "9ae8aa1332b70baf59d0312fd1ecd1a7";
+    let apiKey = "9ae8aa1332b70baf59d0312fd1ecd1a7";
 
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-fetch(url)
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
 
-.then(response => response.json())
+            console.log(data);
 
-.then(data => {
+            if (data.cod != 200) {
+                alert(data.message);
+                return;
+            }
 
-console.log(data);
+            document.querySelector("#weatherResult h2").innerText = data.name;
 
-if(data.cod != 200){
+            document.querySelector("#weatherResult p:nth-of-type(1)").innerText =
+                `Temperature: ${data.main.temp}°C`;
 
-alert(data.message);
+            document.querySelector("#weatherResult p:nth-of-type(2)").innerText =
+                `Condition: ${data.weather[0].main}`;
 
-return;
+            document.getElementById("weatherIcon").src =
+                `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 
-}
+        })
+        .catch(error => {
+            console.log(error);
+            alert("Something went wrong");
+        });
 
-document.querySelector("#weatherResult h2").innerText =
-data.name;
-
-document.querySelector("#weatherResult p:nth-of-type(1)").innerText =
-`Temperature: ${data.main.temp}°C`;
-
-document.querySelector("#weatherResult p:nth-of-type(2)").innerText =
-`Condition: ${data.weather[0].main}`;
-
-document.getElementById("weatherIcon").src =
-`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-
-})
-
-.catch(error => {
-
-console.log(error);
-
-alert("Something went wrong");
-
-});
-
-=======
-let searchBtn = document.getElementById("searchBtn");
-
-searchBtn.addEventListener("click", function () {
-
-let city = document.getElementById("city").value;
-
-if(city === ""){
-alert("Please enter city");
-return;
-}
-
-let apiKey = "9ae8aa1332b70baf59d0312fd1ecd1a7";
-
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-fetch(url)
-
-.then(response => response.json())
-
-.then(data => {
-
-console.log(data);
-
-if(data.cod != 200){
-
-alert(data.message);
-
-return;
-
-}
-
-document.querySelector("#weatherResult h2").innerText =
-data.name;
-
-document.querySelector("#weatherResult p:nth-of-type(1)").innerText =
-`Temperature: ${data.main.temp}°C`;
-
-document.querySelector("#weatherResult p:nth-of-type(2)").innerText =
-`Condition: ${data.weather[0].main}`;
-
-document.getElementById("weatherIcon").src =
-`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-
-})
-
-.catch(error => {
-
-console.log(error);
-
-alert("Something went wrong");
-
-});
-
->>>>>>> 59f002531c5ebf0193b02915410a89bb5da08871
 });
